f733ba6e2a6e3dcbd5e3191dcaea3703e0a43544
rajtilak-patel <rp2380264@gmail.com>
2024-01-08 12:23:42 +0530

    complete singup and login page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 
 import './App.css';
+import LoginPage from './Pages/LoginPage';
+import SignupPage from './Pages/SignupPage';
 import Navbar from './features/navbar/Navbar';
 import Product from './features/product-list/Product';
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (<Link to="/"><Navbar/><Product/></Link>)
+  },
+  {
+    path: "signup",
+    element: (<Link to="/signup"><SignupPage/></Link>),
+  },
+  {
+    path: "login",
+    element: (<Link to="/login"><LoginPage/></Link>),
+  },
+]);
 
 function App() {
   return (
     <div>
-      <Navbar/>
-     <Product/>
+      {/* <Navbar/> */}
+     {/* <Product/> */}
+     {/* <LoginPage/> */}
+     {/* <SignupPage/> */}
+     <RouterProvider router={router} />
     </div>
   );
 }
